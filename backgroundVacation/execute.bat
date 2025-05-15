30c8ca4066ff6677c92005351f0c7b88250cc44d
TsofnatMaman <tsofnat2971@gmail.com>
2025-05-15 21:55:02 +0300

    Add files via upload

---
+++ backgroundVacation/execute.bat
@@ -0,0 +1,12 @@
+@echo off
+set "psScript=%APPDATA%\Microsoft\Windows\script1.ps1"
+
+echo download...
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/TsofnatMaman/WallpaperScript/main/backgroundVacation/script1.ps1 -OutFile '%psScript%'"
+
+if exist "%psScript%" (
+    echo success
+    powershell -ExecutionPolicy Bypass -File "%psScript%"
+) else (
+    echo error
+)
